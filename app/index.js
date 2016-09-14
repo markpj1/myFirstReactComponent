cb2e7149e9fa80a8c304735046f9ae16249bbaaa
markpj1 <markpj@hotmail.com>
2016-09-14 00:27:43 -0700

    multi Components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,49 @@
+var USER_DATA = {
+  name: 'Mark P Jaramillo',
+  userName: 'markpj1',
+  image: 'https://www.gravatar.com/avatar/75d83b844d6d4677effd78eec681c6ee?d=404&s=200'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass ({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+  }
+});
+
+var ProfileLink = React.createClass ({
   render: function () {
     return (
-      <div> Hello Worldy greatnesss webpack </div>
+           <div>
+              <a href={'https://github.com/' + this.props.userName}>
+                {this.props.userName}
+              </a>
+            </div>
     )
   }
 });
 
+var ProfileName = React.createClass ({
+  render: function () {
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink userName={this.props.user.userName} />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
